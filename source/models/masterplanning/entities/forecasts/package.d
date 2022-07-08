4351a074ee87592e785747f69e445b337463b6eb
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-08 07:12:27 +0200

    Update package.d

--- source/models/masterplanning/entities/forecasts/package.d
+++ source/models/masterplanning/entities/forecasts/package.d
@@ -1,1 +1,6 @@
 module models.masterplanning.entities.forecasts;
+
+public import models.masterplanning.entities.forecasts.demandforecastentry;
+public import models.masterplanning.entities.forecasts.model;
+public import models.masterplanning.entities.forecasts.submodel;
+public import models.masterplanning.entities.forecasts.supplyforecastentry;
