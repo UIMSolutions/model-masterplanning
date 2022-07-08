cbd390d8fd3638cda00f835b39c64148191f9fe9
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-08 07:12:54 +0200

    Update package.d

--- source/models/masterplanning/entities/productallocations/package.d
+++ source/models/masterplanning/entities/productallocations/package.d
@@ -1,7 +1,4 @@
 module models.masterplanning.entities.productallocations;
 
-@safe:
-import models.masterplanning;
-
 public import models.masterplanning.entities.productallocations.key;
 public import models.masterplanning.entities.productallocations.line;
