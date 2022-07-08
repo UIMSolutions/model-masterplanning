c6b26faa3fc141945e82e45326c4112d1f5cd78a
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-08 07:23:24 +0200

    Update key.d

--- source/models/masterplanning/entities/productallocations/key.d
+++ source/models/masterplanning/entities/productallocations/key.d
@@ -2,3 +2,25 @@ module models.masterplanning.entities.productallocations.key;
 
 @safe:
 import models.masterplanning;
+
+class DProductAllocationKeyEntity : DOOPEntity {
+  mixin(EntityThis!("ProductAllocationKeyEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([ 
+      ])
+      .registerPath("applications_productallocations.keys");
+  }
+}
+mixin(EntityCalls!("ProductAllocationKeyEntity"));
+
+version(uim_entities) {
+  unittest {
+    assert(ProductAllocationKeyEntity);
+  
+    auto entity = ProductAllocationKeyEntity;
+  }
+}
