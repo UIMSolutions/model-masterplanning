df7989004ec0162f7a8d6dda13b29f812f678344
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-08 07:17:34 +0200

    Update line.d

--- source/models/masterplanning/entities/productallocations/line.d
+++ source/models/masterplanning/entities/productallocations/line.d
@@ -2,3 +2,25 @@ module models.masterplanning.entities.productallocations.line;
 
 @safe:
 import models.masterplanning;
+
+class DProductAllocationLineEntity : DOOPEntity {
+  mixin(EntityThis!("ProductAllocationLineEntity"));
+  
+  override void initialize() {
+    super.initialize;
+
+    this
+      .addValues([ 
+      ])
+      .registerPath("applications_productallocations.lines");
+  }
+}
+mixin(EntityCalls!("AccountEntity"));
+
+version(uim_entities) {
+  unittest {
+    assert(ProductAllocationLineEntity);
+  
+    auto entity = ProductAllocationLineEntity;
+  }
+}
