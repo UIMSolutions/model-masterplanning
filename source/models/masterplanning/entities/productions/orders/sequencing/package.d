f0c206089c1670daa9dffe46f63a359f18a14239
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-11 06:31:57 +0200

    Update package.d

--- source/models/masterplanning/entities/productions/orders/sequencing/package.d
+++ source/models/masterplanning/entities/productions/orders/sequencing/package.d
@@ -1,1 +1,7 @@
 module models.masterplanning.entities.productions.orders.sequencing;
+
+public import models.masterplanning.entities.productions.orders.sequencing.sequence;
+
+public import models.masterplanning.entities.productions.orders.sequencing.group;
+public import models.masterplanning.entities.productions.orders.sequencing.groupline;
+public import models.masterplanning.entities.productions.orders.sequencing.productsequencevalueassignment;
