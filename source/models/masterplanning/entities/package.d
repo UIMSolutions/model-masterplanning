d56596176fd4afbe86c24638c4abd4612d81ab0c
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-11 06:35:17 +0200

    Update package.d

--- source/models/masterplanning/entities/package.d
+++ source/models/masterplanning/entities/package.d
@@ -1,1 +1,8 @@
 module models.masterplanning.entities;
+
+public import models.masterplanning.entities.forecasts;
+public import models.masterplanning.entities.productallocations;
+public import models.masterplanning.entities.productions;
+
+public import models.masterplanning.entities.leanproductleanschedulegroupassignment;
+public import models.masterplanning.entities.leanschedulegroup;
